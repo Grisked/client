8755c617bc3d83642a54565754c7d9edb6f9589f
RqndomHax <paul.codepro@gmail.com>
2022-11-20 19:20:37 +0100

    :lipstick: Home page now contains default informations

--- grisked-ui/src/app.rs
+++ grisked-ui/src/app.rs
@@ -1,5 +1,7 @@
-use iced::widget::{button, column, container, row, Column};
-use iced::{executor, theme};
+use iced::alignment::{Horizontal, Vertical};
+use iced::theme::Container;
+use iced::widget::{self, button, column, container, row, text, Column};
+use iced::{executor, theme, Alignment};
 use iced::{Application, Command, Element, Length, Settings, Theme};
 
 use crate::entity::menu::*;
@@ -44,7 +46,7 @@ impl Application for Grisked {
     }
 
     fn view(&self) -> Element<Message> {
-        let choose_menu = [
+        let sidebar = [
             MenuType::Home,
             MenuType::Accounts,
             MenuType::Charts,
@@ -53,23 +55,86 @@ impl Application for Grisked {
         ]
         .iter()
         .fold(
-            Column::new().spacing(10).height(Length::Fill),
+            Column::new()
+                .spacing(10)
+                .height(Length::Fill)
+                .width(Length::FillPortion(1)),
             |column, option| {
                 column.push(
                     button(row![option.get_icon(), option.get_name(&self.language)].spacing(10))
                         .on_press(crate::Message::MenuChanged(*option))
                         .padding(10)
-                        .style(theme::Button::Primary),
+                        .style(theme::Button::Primary)
+                        .width(Length::Fill),
                 )
             },
         );
 
-        let content = column![choose_menu];
-
-        container(content)
+        let sidebar_container: Element<Message> = container(sidebar)
             .width(Length::Fill)
             .height(Length::Fill)
             .padding(20)
-            .into()
+            .style(theme::Container::Box)
+            .into();
+
+        let context = match self.menu_type {
+            MenuType::Home => {
+                let value = Column::new()
+                    .spacing(10)
+                    .push(
+                        container(column!(
+                            text("Comptes récemments utilisés"),
+                            row!(
+                                column!(text("Compte 1"), text("Livret A"))
+                                    .align_items(Alignment::Start),
+                                column!(text("834€"), text("7493€")).align_items(Alignment::End)
+                            )
+                        ))
+                        .style(Container::Box)
+                        .padding(20),
+                    )
+                    .push(
+                        container(column!(
+                            text("Echéances en cours"),
+                            row!(text("Prêt de la 4090 RTX"), text("10384€")),
+                        ))
+                        .style(Container::Box)
+                        .padding(20),
+                    )
+                    .push(
+                        container(column!(
+                            text("Rappels"),
+                            row!(text("Payer la facture d'eau du 11/04/23"))
+                        ))
+                        .style(Container::Box)
+                        .padding(20),
+                    );
+
+                let container: Element<Message> = container(value)
+                    .width(Length::FillPortion(3))
+                    .height(Length::Fill)
+                    .padding(50)
+                    .into();
+
+                Some(container)
+            }
+            _ => None,
+        };
+
+        match context {
+            Some(context) => {
+                let content = row![sidebar_container, context];
+                container(content)
+                    .width(Length::Fill)
+                    .height(Length::Fill)
+                    .padding(20)
+                    .into()
+            }
+            None => container(sidebar_container)
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .padding(20)
+                .into(),
+        }
     }
 }
