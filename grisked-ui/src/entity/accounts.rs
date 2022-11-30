7a7d5fb5e5ec498c1e75da49da8cf4cada598d68
Guismus <Guillaume.boudaille@epitech.eu>
2022-11-30 17:25:52 +0100

    :lipstick: Accounts page V2

--- grisked-ui/src/entity/accounts.rs
+++ grisked-ui/src/entity/accounts.rs
@@ -1,11 +1,11 @@
 use grisked_profile::profile::Profile;
 use iced::{
     alignment, theme,
-    widget::{column, container, row, text, Column, Container},
-    Length,
+    widget::{column, button, text_input, container, row, text, Column, Container, Canvas},
+    Length, Color, alignment::Alignment
 };
 
-use crate::{stylesheet::ContainerType, view::View, Message};
+use crate::{stylesheet::{ContainerType, label_square::LabelSquare}, view::View, Message, font::{FontFamily, FontType}};
 
 pub fn accounts(profile: &Profile, view: &View) -> Container<'static, Message> {
     let center = Column::new().spacing(10).push(list_accounts(profile, view));
@@ -20,31 +20,91 @@ pub fn accounts(profile: &Profile, view: &View) -> Container<'static, Message> {
 
 fn list_accounts(_profile: &Profile, _view: &View) -> Container<'static, Message> {
     container(column!(
-        text("Comptes courants")
-            .horizontal_alignment(alignment::Horizontal::Center)
+        FontType::Title
+            .get_text("Mes comptes".to_string(), FontFamily::IndieFlower)
             .width(Length::Fill)
-            .size(30),
-        row!(text(" ")),
+            .style(Color::from([0.2235, 0.0, 0.5294]))
+            .size(45)
+            .horizontal_alignment(alignment::Horizontal::Center),
         row!(
-            text("Compte 1"),
-            text("834€")
-                .horizontal_alignment(alignment::Horizontal::Right)
-                .width(Length::Fill)
-        )
-        .spacing(25),
+        button(text("<").horizontal_alignment(alignment::Horizontal::Center).width(Length::Fill).size(30)).width(Length::FillPortion(1)),
+        Canvas::new(LabelSquare::new([1.0, 0.0, 0.0])).width(Length::FillPortion(1)),
+        Canvas::new(LabelSquare::new([1.0, 0.0, 0.0])).width(Length::FillPortion(1)),
+        Canvas::new(LabelSquare::new([1.0, 0.0, 0.0])).width(Length::FillPortion(1)),
+        button(text(">").horizontal_alignment(alignment::Horizontal::Center).width(Length::Fill).size(30)).width(Length::FillPortion(1)),
+        ).spacing(10).align_items(Alignment::Center),
+        text(" "),
+        text(" "),
         row!(
-            text("Compte 2"),
-            text("7493€")
-                .horizontal_alignment(alignment::Horizontal::Right)
-                .width(Length::Fill)
-        )
-        .spacing(25),
-        row!(text(" ")),
-        row!(text("Total courants: 45678€")
-            .horizontal_alignment(alignment::Horizontal::Right)
-            .width(Length::Fill)
-            .size(15)),
+            FontType::Title
+                .get_text("Ajouter un compte".to_string(), FontFamily::IndieFlower)
+                .width(Length::FillPortion(7))
+                .style(Color::from([0.2235, 0.0, 0.5294]))
+                .size(30)
+                .horizontal_alignment(alignment::Horizontal::Center),
+            text(" ").width(Length::FillPortion(1)),
+            FontType::Title
+                .get_text("Créer un label".to_string(), FontFamily::IndieFlower)
+                .width(Length::FillPortion(7))
+                .style(Color::from([0.2235, 0.0, 0.5294]))
+                .size(30)
+                .horizontal_alignment(alignment::Horizontal::Center),
+        ).spacing(50),
+        row!(
+            container(column!(
+                row!(
+                    container(column!(
+                        container(column!(
+                            text("Nom du compte"),
+                        )).style(theme::Container::Custom(ContainerType::Box.get_box())).width(Length::FillPortion(7)).padding(10),
+                        text(" "),
+                        row!(
+                            container(column!(
+                                row!(
+                                text("Solde"),
+                                text("€").horizontal_alignment(alignment::Horizontal::Right).width(Length::Fill),
+                                )
+                            )).style(theme::Container::Custom(ContainerType::Box.get_box())).width(Length::FillPortion(1)).padding(10),
+                            container(column!(
+                                row!(
+                                    text("Couleur").width(Length::Fill),
+                                    Canvas::new(LabelSquare::new([1.0, 0.0, 0.0])).width(Length::Units(20)).height(Length::Units(20)),
+                                    )
+                            )).style(theme::Container::Custom(ContainerType::Box.get_box())).width(Length::FillPortion(1)).padding(10),
+                        ).spacing(10),
+                    )).width(Length::Fill).padding(10),
+                    button("+"),
+                ).align_items(Alignment::Center),
+            )).style(theme::Container::Custom(ContainerType::Box.get_box())).width(Length::FillPortion(7)).padding(10),
+            text(" ").width(Length::FillPortion(1)),
+            container(column!(
+                text("tkt"),
+            )).style(theme::Container::Custom(ContainerType::Box.get_box())).width(Length::FillPortion(7)).padding(10),
+        ).spacing(50),
+        row!(
+            FontType::Title
+                .get_text("Ajouter une facture".to_string(), FontFamily::IndieFlower)
+                .width(Length::FillPortion(7))
+                .style(Color::from([0.2235, 0.0, 0.5294]))
+                .size(30)
+                .horizontal_alignment(alignment::Horizontal::Center),
+            text(" ").width(Length::FillPortion(1)),
+            FontType::Title
+                .get_text("Ajouter une entrée".to_string(), FontFamily::IndieFlower)
+                .width(Length::FillPortion(7))
+                .style(Color::from([0.2235, 0.0, 0.5294]))
+                .size(30)
+                .horizontal_alignment(alignment::Horizontal::Center),
+        ).spacing(50),
+        row!(
+            container(column!(
+                text("tkt"),
+            )).style(theme::Container::Custom(ContainerType::Box.get_box())).width(Length::FillPortion(7)).padding(10),
+            text(" ").width(Length::FillPortion(1)),
+            container(column!(
+                text("tkt"),
+            )).style(theme::Container::Custom(ContainerType::Box.get_box())).width(Length::FillPortion(7)).padding(10),
+        ).spacing(50),
     ))
-    .style(theme::Container::Custom(ContainerType::Box.get_box()))
     .padding(20)
 }
