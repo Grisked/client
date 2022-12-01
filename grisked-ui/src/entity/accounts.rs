eb6ba8a43a61003a4692e6e188927100efb80109
Paul COMTE <paul.codepro@gmail.com>
2022-12-01 22:04:26 +0100

    :art: beautify ui/accounts code

--- grisked-ui/src/entity/accounts.rs
+++ grisked-ui/src/entity/accounts.rs
@@ -15,12 +15,15 @@ use crate::{
 };
 
 pub fn accounts(profile: &Profile, view: &View) -> Container<'static, Message> {
-    let center = Column::new().spacing(10).push(list_accounts(profile, view));
+    let accounts = list_accounts(profile, view);
+    let top = top_side(profile, view);
+    let bottom = bottom_side(profile, view);
 
-    let container: Container<Message> = container(row!(center).spacing(20))
-        .height(Length::FillPortion(7))
-        .width(Length::Fill)
-        .padding(50);
+    let container: Container<Message> =
+        container(column!(accounts, column!(top, bottom).spacing(50)))
+            .height(Length::FillPortion(7))
+            .width(Length::Fill)
+            .padding(50);
     container
 }
 
@@ -103,6 +106,13 @@ fn list_accounts(_profile: &Profile, _view: &View) -> Container<'static, Message
         // Weird wish spacing
         text(" "),
         text(" "),
+        row!(text(" ")),
+        row!(text(" ")),
+    ))
+}
+
+fn top_side(profile: &Profile, view: &View) -> Column<'static, Message> {
+    column!(
         // Titres du haut
         row!(
             FontType::Title
@@ -119,79 +129,12 @@ fn list_accounts(_profile: &Profile, _view: &View) -> Container<'static, Message
                 .horizontal_alignment(alignment::Horizontal::Center),
         )
         .spacing(50),
-        row!(
-            // Case en haut à gauche
-            container(column!(row!(
-                container(column!(
-                    container(column!(text("Nom du compte"),))
-                        .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                        .width(Length::FillPortion(7))
-                        .padding(10),
-                    text(" "),
-                    row!(
-                        container(column!(row!(
-                            text("Solde"),
-                            text("€")
-                                .horizontal_alignment(alignment::Horizontal::Right)
-                                .width(Length::Fill),
-                        )))
-                        .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                        .width(Length::FillPortion(1))
-                        .padding(10),
-                        container(column!(row!(
-                            text("Couleur").width(Length::Fill),
-                            Canvas::new(LabelSquare::new([1.0, 0.0, 0.0]))
-                                .width(Length::Units(20))
-                                .height(Length::Units(20)),
-                        )))
-                        .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                        .width(Length::FillPortion(1))
-                        .padding(10),
-                    )
-                    .spacing(10),
-                ))
-                .width(Length::Fill)
-                .padding(10),
-                button(svg(svg::Handle::from_path("assets/add_button.svg")))
-                    .style(theme::Button::Custom(ButtonType::BoxIgnored.get_box()))
-                    .width(Length::Units(48)),
-            )
-            .align_items(Alignment::Center),))
-            .style(theme::Container::Custom(ContainerType::Box.get_box()))
-            .width(Length::FillPortion(7))
-            .padding(10),
-            // Case au haut à droite
-            container(column!(row!(
-                container(column!(
-                    container(column!(text("Nom du label"),))
-                        .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                        .width(Length::FillPortion(7))
-                        .padding(10),
-                    text(" "),
-                    container(column!(row!(
-                        text("Couleur").width(Length::Fill),
-                        Canvas::new(LabelSquare::new([1.0, 0.0, 0.0]))
-                            .width(Length::Units(20))
-                            .height(Length::Units(20)),
-                    )))
-                    .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                    .width(Length::FillPortion(1))
-                    .padding(10),
-                ))
-                .width(Length::Fill)
-                .padding(10),
-                button(svg(svg::Handle::from_path("assets/add_button.svg")))
-                    .style(theme::Button::Custom(ButtonType::BoxIgnored.get_box()))
-                    .width(Length::Units(48)),
-            )
-            .align_items(Alignment::Center),))
-            .style(theme::Container::Custom(ContainerType::Box.get_box()))
-            .width(Length::FillPortion(7))
-            .padding(10),
-        )
-        .spacing(50),
-        row!(text(" ")),
-        row!(text(" ")),
+        row!(add_account(profile, view), add_label(profile, view)).spacing(50)
+    )
+}
+
+fn bottom_side(profile: &Profile, view: &View) -> Column<'static, Message> {
+    column!(
         // Titres du bas
         row!(
             FontType::Title
@@ -201,96 +144,174 @@ fn list_accounts(_profile: &Profile, _view: &View) -> Container<'static, Message
                 .size(30)
                 .horizontal_alignment(alignment::Horizontal::Center),
             FontType::Title
-                .get_text("Ajouter une entrée".to_string(), FontFamily::IndieFlower)
+                .get_text("Ajouter un revenu".to_string(), FontFamily::IndieFlower)
                 .width(Length::FillPortion(7))
                 .style(Color::from([0.2235, 0.0, 0.5294]))
                 .size(30)
                 .horizontal_alignment(alignment::Horizontal::Center),
         )
         .spacing(50),
-        row!(
-            // Case en bas à gauche
-            container(column!(row!(
-                container(column!(
-                    row!(
-                        container(column!(text("Nom de la facture"),))
-                            .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                            .width(Length::FillPortion(2))
-                            .padding(10),
-                        container(column!(row!(
-                            text("Montant"),
-                            text("€")
-                                .horizontal_alignment(alignment::Horizontal::Right)
-                                .width(Length::Fill),
-                        )))
-                        .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                        .width(Length::FillPortion(1))
-                        .padding(10),
-                    )
-                    .spacing(10),
-                    text(" "),
-                    row!(
-                        container(column!(text("DD/MM/YYYY"),))
-                            .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                            .width(Length::FillPortion(2))
-                            .padding(10),
-                        container(column!(text("DD/MM/YYYY"),))
-                            .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                            .width(Length::FillPortion(2))
-                            .padding(10),
-                        container(column!(text("Label"),))
-                            .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                            .width(Length::FillPortion(2))
-                            .padding(10),
-                    )
-                    .spacing(10),
-                ))
-                .width(Length::Fill)
+        row!(add_invoice(profile, view), add_income(profile, view)).spacing(50)
+    )
+}
+
+fn add_label(_profile: &Profile, _view: &View) -> Container<'static, Message> {
+    // Case au haut à droite
+    container(column!(row!(
+        container(column!(
+            container(column!(text("Nom du label"),))
+                .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                .width(Length::FillPortion(7))
                 .padding(10),
-                button(svg(svg::Handle::from_path("assets/add_button.svg")))
-                    .style(theme::Button::Custom(ButtonType::BoxIgnored.get_box()))
-                    .width(Length::Units(48)),
-            )
-            .align_items(Alignment::Center),))
-            .style(theme::Container::Custom(ContainerType::Box.get_box()))
-            .width(Length::FillPortion(7))
-            .padding(10),
-            // Case en bas à droite
+            text(" "),
             container(column!(row!(
-                container(column!(
-                    container(column!(text("Nom de la facture"),))
-                        .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                        .width(Length::FillPortion(7))
-                        .padding(10),
-                    text(" "),
-                    row!(
-                        container(column!(row!(
-                            text("Montant"),
-                            text("€")
-                                .horizontal_alignment(alignment::Horizontal::Right)
-                                .width(Length::Fill),
-                        )))
-                        .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                        .width(Length::FillPortion(1))
-                        .padding(10),
-                        container(column!(text("DD/MM/YYYY").width(Length::Fill),))
-                            .style(theme::Container::Custom(ContainerType::Box.get_box()))
-                            .width(Length::FillPortion(1))
-                            .padding(10),
-                    )
-                    .spacing(10),
-                ))
-                .width(Length::Fill)
-                .padding(10),
-                button(svg(svg::Handle::from_path("assets/add_button.svg")))
-                    .style(theme::Button::Custom(ButtonType::BoxIgnored.get_box()))
-                    .width(Length::Units(48)),
-            )
-            .align_items(Alignment::Center),))
+                text("Couleur").width(Length::Fill),
+                Canvas::new(LabelSquare::new([1.0, 0.0, 0.0]))
+                    .width(Length::Units(20))
+                    .height(Length::Units(20)),
+            )))
             .style(theme::Container::Custom(ContainerType::Box.get_box()))
-            .width(Length::FillPortion(7))
+            .width(Length::FillPortion(1))
             .padding(10),
-        )
-        .spacing(50),
-    ))
+        ))
+        .width(Length::Fill)
+        .padding(10),
+        button(svg(svg::Handle::from_path("assets/add_button.svg")))
+            .style(theme::Button::Custom(ButtonType::BoxIgnored.get_box()))
+            .width(Length::Units(48)),
+    )
+    .align_items(Alignment::Center),))
+    .style(theme::Container::Custom(ContainerType::Box.get_box()))
+    .width(Length::FillPortion(7))
+    .padding(10)
+}
+
+fn add_account(_profile: &Profile, _view: &View) -> Container<'static, Message> {
+    // Case en haut à gauche
+    container(column!(row!(
+        container(column!(
+            container(column!(text("Nom du compte"),))
+                .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                .width(Length::FillPortion(7))
+                .padding(10),
+            text(" "),
+            row!(
+                container(column!(row!(
+                    text("Solde"),
+                    text("€")
+                        .horizontal_alignment(alignment::Horizontal::Right)
+                        .width(Length::Fill),
+                )))
+                .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                .width(Length::FillPortion(1))
+                .padding(10),
+                container(column!(row!(
+                    text("Couleur").width(Length::Fill),
+                    Canvas::new(LabelSquare::new([1.0, 0.0, 0.0]))
+                        .width(Length::Units(20))
+                        .height(Length::Units(20)),
+                )))
+                .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                .width(Length::FillPortion(1))
+                .padding(10),
+            )
+            .spacing(10),
+        ))
+        .width(Length::Fill)
+        .padding(10),
+        button(svg(svg::Handle::from_path("assets/add_button.svg")))
+            .style(theme::Button::Custom(ButtonType::BoxIgnored.get_box()))
+            .width(Length::Units(48)),
+    )
+    .align_items(Alignment::Center),))
+    .style(theme::Container::Custom(ContainerType::Box.get_box()))
+    .width(Length::FillPortion(7))
+    .padding(10)
+}
+
+fn add_income(_profile: &Profile, _view: &View) -> Container<'static, Message> {
+    // Case en bas à droite
+    container(column!(row!(
+        container(column!(
+            container(column!(text("Nom de la facture"),))
+                .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                .width(Length::FillPortion(7))
+                .padding(10),
+            text(" "),
+            row!(
+                container(column!(row!(
+                    text("Montant"),
+                    text("€")
+                        .horizontal_alignment(alignment::Horizontal::Right)
+                        .width(Length::Fill),
+                )))
+                .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                .width(Length::FillPortion(1))
+                .padding(10),
+                container(column!(text("DD/MM/YYYY").width(Length::Fill),))
+                    .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                    .width(Length::FillPortion(1))
+                    .padding(10),
+            )
+            .spacing(10),
+        ))
+        .width(Length::Fill)
+        .padding(10),
+        button(svg(svg::Handle::from_path("assets/add_button.svg")))
+            .style(theme::Button::Custom(ButtonType::BoxIgnored.get_box()))
+            .width(Length::Units(48)),
+    )
+    .align_items(Alignment::Center),))
+    .style(theme::Container::Custom(ContainerType::Box.get_box()))
+    .width(Length::FillPortion(7))
+    .padding(10)
+}
+
+fn add_invoice(_profile: &Profile, _view: &View) -> Container<'static, Message> {
+    // Case en bas à gauche
+    container(column!(row!(
+        container(column!(
+            row!(
+                container(column!(text("Nom de la facture"),))
+                    .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                    .width(Length::FillPortion(2))
+                    .padding(10),
+                container(column!(row!(
+                    text("Montant"),
+                    text("€")
+                        .horizontal_alignment(alignment::Horizontal::Right)
+                        .width(Length::Fill),
+                )))
+                .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                .width(Length::FillPortion(1))
+                .padding(10),
+            )
+            .spacing(10),
+            text(" "),
+            row!(
+                container(column!(text("DD/MM/YYYY"),))
+                    .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                    .width(Length::FillPortion(2))
+                    .padding(10),
+                container(column!(text("DD/MM/YYYY"),))
+                    .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                    .width(Length::FillPortion(2))
+                    .padding(10),
+                container(column!(text("Label"),))
+                    .style(theme::Container::Custom(ContainerType::Box.get_box()))
+                    .width(Length::FillPortion(2))
+                    .padding(10),
+            )
+            .spacing(10),
+        ))
+        .width(Length::Fill)
+        .padding(10),
+        button(svg(svg::Handle::from_path("assets/add_button.svg")))
+            .style(theme::Button::Custom(ButtonType::BoxIgnored.get_box()))
+            .width(Length::Units(48)),
+    )
+    .align_items(Alignment::Center),))
+    .style(theme::Container::Custom(ContainerType::Box.get_box()))
+    .width(Length::FillPortion(7))
+    .padding(10)
 }
