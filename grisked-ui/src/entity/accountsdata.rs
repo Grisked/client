097f8b0528adf95c2b825d1f3a0c782b5f060c1b
Guismus <guillaume.boudaille@epitech.eu>
2022-12-05 17:14:45 +0100

    payements/incomes container designed

--- grisked-ui/src/entity/accountsdata.rs
+++ grisked-ui/src/entity/accountsdata.rs
@@ -17,7 +17,7 @@ use crate::{
     entity::menu::MenuType,
     font::{FontFamily, FontType},
     stylesheet::{label_square::LabelSquare, ButtonType, ContainerType},
-    view::View,
+    view::{View, ViewSize},
     FieldSettings, Message, UpdateBox,
 };
 
@@ -50,26 +50,79 @@ fn top_side(
         .horizontal_alignment(alignment::Horizontal::Center),)
 }
 
-fn bottom_side(_profile: &Profile, _view: &View) -> Column<'static, Message> {
+fn bottom_side(_profile: &Profile, view: &View) -> Column<'static, Message> {
     column!(
         // Titres du bas
         row!(
-            container(row!(
-                FontType::Text
-                    .get_text("Factures".to_string(), FontFamily::IndieFlower)
+            container(column!(
+                row!(
+                    FontType::Title
+                        .get_text("Factures".to_string(), FontFamily::IndieFlower)
+                        .width(Length::Fill)
+                        .style(Color::from([0.2235, 0.0, 0.5294]))
+                        .size(ViewSize::Title.get_size(view)),
+                    FontType::TextBold
+                        .get_text("1073.00€".to_string(), FontFamily::Kanit)
+                        .width(Length::Fill)
+                        .style(Color::from([0.0, (134.0 / 255.0), 0.0]))
+                        .size(ViewSize::Title.get_size(view))
+                        .horizontal_alignment(alignment::Horizontal::Right),
+                )
+                .padding([0, 0, 10, 0]),
+                {
+                    let mut column = Column::new();
+
+                    for _ in 0..10 {
+                        column = column.push(
+                            row!(
+                                FontType::Text
+                                    .get_text("[MonLabel]".to_string(), FontFamily::Kanit)
+                                    .size(ViewSize::Text.get_size(view)),
+                                FontType::TextBold
+                                    .get_text("Facture du facteur".to_string(), FontFamily::Kanit)
+                                    .size(ViewSize::Text.get_size(view)),
+                                FontType::Text
+                                    .get_text("09/12/2004".to_string(), FontFamily::Kanit)
+                                    .size(ViewSize::Text.get_size(view)),
+                                svg(svg::Handle::from_path("assets/arrow-right-short.svg"))
+                                    .height(Length::Units(ViewSize::Text.get_size(view)))
+                                    .width(Length::Units(ViewSize::Text.get_size(view))),
+                                FontType::Text
+                                    .get_text("10/01/2005".to_string(), FontFamily::Kanit)
+                                    .size(ViewSize::Text.get_size(view)),
+                                FontType::TextBold
+                                    .get_text("1139.12€".to_string(), FontFamily::Kanit)
+                                    .size(ViewSize::Text.get_size(view)),
+                            )
+                            .spacing(10),
+                        );
+                    }
+
+                    column
+                },
+                row!(FontType::Title
+                    .get_text("Revenus".to_string(), FontFamily::IndieFlower)
                     .width(Length::Fill)
                     .style(Color::from([0.2235, 0.0, 0.5294]))
-                    .size(30),
-                FontType::TextBold
-                    .get_text("1073.00€".to_string(), FontFamily::Kanit)
-                    .width(Length::Fill)
-                    .style(Color::from([0.0, (134.0 / 255.0), 0.0]))
-                    .size(30)
-                    .horizontal_alignment(alignment::Horizontal::Right),
+                    .size(ViewSize::Title.get_size(view)),)
+                .padding([10, 0, 10, 0]),
+                row!(
+                    FontType::TextBold
+                        .get_text("La mère grand met bien".to_string(), FontFamily::Kanit)
+                        .size(ViewSize::Text.get_size(view)),
+                    FontType::Text
+                        .get_text("09/12/2004".to_string(), FontFamily::Kanit)
+                        .size(ViewSize::Text.get_size(view)),
+                    FontType::TextBold
+                        .get_text("1139.12€".to_string(), FontFamily::Kanit)
+                        .style(Color::from([0.0, (134.0 / 255.0), 0.0]))
+                        .size(ViewSize::Text.get_size(view)),
+                )
+                .spacing(10)
             ))
             .style(theme::Container::Custom(ContainerType::Box.get_box()))
             .padding(10)
-            .width(Length::FillPortion(1)),
+            .width(Length::FillPortion(2)),
             container(
                 FontType::Title
                     .get_text("Ajouter un revenu".to_string(), FontFamily::IndieFlower)
